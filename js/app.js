3b91e6c400426e060b759bbe191bd8c391954405
LuciLua <luci.lua81@gmail.com>
2021-07-23 22:50:50 -0300

    :cactus: animation toggle window app

--- js/app.js
+++ js/app.js
@@ -9,8 +9,15 @@ function log(message){
 }
 
 const appConsole = document.querySelectorAll('.console')
+const appBrowser = document.querySelectorAll('.browser')
 
 appConsole.forEach(e => {
+    e.addEventListener('dragstart', dragstart)
+    e.addEventListener('drag', drag)
+    e.addEventListener('dragend', dragend)
+})
+
+appBrowser.forEach(e => {
     e.addEventListener('dragstart', dragstart)
     e.addEventListener('drag', drag)
     e.addEventListener('dragend', dragend)
@@ -65,13 +72,30 @@ function dragdrop(){
 
 // console open
 
+function openConsole(){
+
+}
+
+
 function toggleConsole(){
+    
     const actionConsole = document.querySelectorAll('.console')
     
     actionConsole.forEach(e => {
         e.classList.toggle('consolef')
-        // e.style.backgroundColor= 'rgba(56, 56, 56, 0.663)'
+        e.style.animation='toggle 0.2s linear'
     })
+    
+    var space = document.getElementById('screen-action')
+    for (let i = 0; i < actionConsole.length; i++){
+        // space.innerHTML += "<div class='consolef'></div>"
+    }
+}
 
-
+function toggleBrowser(){
+    const actionConsole = document.querySelectorAll('.browser')
+    actionConsole.forEach(e => {
+        e.classList.toggle('browserf')
+        e.style.animation='toggle 0.2s linear'
+    })
 }
